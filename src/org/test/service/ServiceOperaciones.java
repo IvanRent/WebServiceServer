3f6a709062f558cd1af1fdaf27b1bd454df283d6
IvanRent <ivan_darenteri@hotmail.com>
2019-08-13 12:20:25 -0500

    Add files via upload

---
+++ src/org/test/service/ServiceOperaciones.java
@@ -0,0 +1,15 @@
+package org.test.service;
+
+public class ServiceOperaciones {
+	 public String operacionesMulti(int c, int d) 
+	 {
+	    int multi = c + d; 
+	    return "Multiplicacion= " + multi;
+	      
+	  }
+	 public String operacionSuma( int a, int b)
+	 	{
+			int suma =a + b;
+			return  "SUMA= "+ suma;
+		}
+}
